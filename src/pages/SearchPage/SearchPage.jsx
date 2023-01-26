245c1f0c6984245426974978b2aeaf4514bb350b
marta szuran <marta.szuran@dataart.com>
2023-01-26 08:55:53 +0100

    search query and reclaim URL query

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,3 +1,20 @@
+import queryString from 'query-string';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { fetchSearchData, selectAllSeachData } from '../../redux/slices/searchDataSlice';
+import { MOVIES, PEOPLE, TV } from '../../constants/searchTypes';
+import './SearchPage.css';
+
 export default function SearchPage() {
+  const dispatch = useDispatch();
+  const searchData = useSelector(selectAllSeachData);
+  const { query } = queryString.parse(window.location.search);
+  const [searchQuery, setSearchQuery] = useState(query);
+  const [searchType, setSearchType] = useState(MOVIES);
+
+  useEffect(() => {
+    dispatch(fetchSearchData({ searchQuery, searchType }));
+  }, [searchQuery, searchType]);
+
   return (<div>Search Page</div>);
 }
